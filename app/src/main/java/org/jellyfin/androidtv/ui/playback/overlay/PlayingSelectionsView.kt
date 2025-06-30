f2e39097b66d1de26f76366dab54a455e6aae863
lovewlever <35085409+lovewlever@users.noreply.github.com>
2025-06-30 11:49:42 +0800

    Refactor: Improve focus management in playback selections
    This commit refactors the focus management within the `PlayingSelectionsView` in the playback overlay.
    
    - When the `PlayingSelectionsView` gains focus, it now attempts to focus the currently playing item if visible.
    - If the current item is not visible, it will try to focus the previously focused item.
    - If neither of the above is possible, it defaults to focusing the first visible item in the list.
    - The text style for item titles now uses `MaterialTheme.typography.bodyMedium`.

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/overlay/PlayingSelectionsView.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/overlay/PlayingSelectionsView.kt
@@ -20,12 +20,13 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
@@ -43,35 +44,25 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.graphics.toColor
-import androidx.fragment.app.FragmentActivity
+import androidx.core.graphics.toColorInt
 import coil3.compose.AsyncImage
 import coil3.request.CachePolicy
 import coil3.request.ImageRequest
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
 import org.jellyfin.androidtv.data.repository.ItemRepository
 import org.jellyfin.androidtv.preference.UserPreferences
-import org.jellyfin.androidtv.preference.constant.AppTheme
 import org.jellyfin.androidtv.ui.base.JellyfinTheme
 import org.jellyfin.androidtv.ui.base.Text
-import org.jellyfin.androidtv.ui.preference.screen.UserPreferencesScreen
-import org.jellyfin.androidtv.util.ThemeViewModel
 import org.jellyfin.androidtv.util.apiclient.getUrl
 import org.jellyfin.androidtv.util.apiclient.itemImages
-import org.jellyfin.androidtv.util.applyTheme
 import org.jellyfin.sdk.api.client.ApiClient
 import org.jellyfin.sdk.api.client.extensions.itemsApi
 import org.jellyfin.sdk.model.api.BaseItemDto
 import org.jellyfin.sdk.model.api.request.GetItemsRequest
 import org.koin.compose.koinInject
-import java.util.concurrent.atomic.AtomicReference
-import androidx.core.graphics.toColorInt
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.onEach
 import timber.log.Timber
 
 
@@ -79,144 +70,181 @@ import timber.log.Timber
  * Provides the function of selecting episodes in the playback page
  */
 internal fun createPlayingSelectionsView(
-	context: Context,
-	currentItem: BaseItemDto,
-	onItemClicked: (Int, List<BaseItemDto>) -> Unit?,
+    context: Context,
+    currentItem: BaseItemDto,
+    onItemClicked: (Int, List<BaseItemDto>) -> Unit?,
 ): ComposeView {
-	var viewHasFocused by mutableStateOf(false)
-	return ComposeView(context).apply {
-		isFocusable = true
-		setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
-		this.onFocusChangeListener = OnFocusChangeListener { v, hasFocus -> viewHasFocused = hasFocus }
+    var viewHasFocused by mutableStateOf(false)
+    return ComposeView(context).apply {
+        isFocusable = true
+        setLayoutParams(
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        )
+        this.onFocusChangeListener =
+            OnFocusChangeListener { v, hasFocus -> viewHasFocused = hasFocus }
 
-		setContent {
-			JellyfinTheme {
-				PlayingSelectionsView(
-					modifier = Modifier.padding(top = 8.dp),
-					viewHasFocused = viewHasFocused,
-					currentItem = currentItem,
-					onItemClicked = onItemClicked
-				)
-			}
-		}
-	}
+        setContent {
+            JellyfinTheme {
+                PlayingSelectionsView(
+                    modifier = Modifier.padding(top = 8.dp),
+                    viewHasFocused = viewHasFocused,
+                    currentItem = currentItem,
+                    onItemClicked = onItemClicked
+                )
+            }
+        }
+    }
 }
 
 
 @Composable
 private fun PlayingSelectionsView(
-	modifier: Modifier = Modifier,
-	viewHasFocused: Boolean,
-	currentItem: BaseItemDto,
-	onItemClicked: (Int, List<BaseItemDto>) -> Unit?,
+    modifier: Modifier = Modifier,
+    viewHasFocused: Boolean,
+    currentItem: BaseItemDto,
+    onItemClicked: (Int, List<BaseItemDto>) -> Unit?,
 ) {
-	val itemsToPlayStateList = remember { mutableStateListOf<BaseItemDto>() }
-	var first by remember { mutableStateOf(true) }
-	val api = koinInject<ApiClient>()
-	val lazyRowState = rememberLazyListState()
-	val context = LocalContext.current
+    val itemsToPlayStateList = remember { mutableStateListOf<BaseItemDto>() }
+    val api = koinInject<ApiClient>()
+    val lazyRowState = rememberLazyListState()
+    var currentFocusedIndex by remember { mutableIntStateOf(-1) }
+    val context = LocalContext.current
+    val focusRequesterStateList = remember(itemsToPlayStateList.size) {
+        List(itemsToPlayStateList.size) { FocusRequester() }
+    }
 
-	val composeColor = remember {
-		val attrs = context.obtainStyledAttributes(UserPreferences.appTheme.defaultValue.nameRes, intArrayOf(android.R.attr.colorAccent))
-		val color = attrs.getColor(0, "#FFFFFF".toColorInt())
-		val composeColor = Color(color)
-		attrs.recycle()
-		composeColor
-	}
+    val composeColor = remember {
+        val attrs = context.obtainStyledAttributes(
+            UserPreferences.appTheme.defaultValue.nameRes,
+            intArrayOf(android.R.attr.colorAccent)
+        )
+        val color = attrs.getColor(0, "#FFFFFF".toColorInt())
+        val composeColor = Color(color)
+        attrs.recycle()
+        composeColor
+    }
 
-	LaunchedEffect(currentItem) {
-		val byName = GetItemsRequest(
-			fields = ItemRepository.itemFields,
-			parentId = currentItem.seasonId,
-		)
-		val response = withContext(Dispatchers.IO) {
-			api.itemsApi.getItems(byName).content
-		}
-		itemsToPlayStateList.clear()
-		itemsToPlayStateList.addAll(response.items)
+    LaunchedEffect(currentItem) {
+        val byName = GetItemsRequest(
+            fields = ItemRepository.itemFields,
+            parentId = currentItem.seasonId,
+        )
+        val response = withContext(Dispatchers.IO) {
+            api.itemsApi.getItems(byName).content
+        }
+        itemsToPlayStateList.clear()
+        itemsToPlayStateList.addAll(response.items)
 
-		itemsToPlayStateList.indexOfFirst { it.id == currentItem.id }.takeIf { it > 0 }?.let {
-			lazyRowState.scrollToItem(it)
-		}
-	}
+        itemsToPlayStateList.indexOfFirst { it.id == currentItem.id }.takeIf { it > 0 }?.let { targetIndex ->
+            snapshotFlow { lazyRowState.layoutInfo.totalItemsCount }
+                .filter { it > targetIndex }
+                .first()
+            lazyRowState.scrollToItem(targetIndex)
+        }
+    }
 
-	LazyRow(
-		modifier = modifier
-			.focusGroup()
-			.focusable(),
-		state = lazyRowState
-	) {
-		itemsIndexed(itemsToPlayStateList, key = { index, item -> item.id }, contentType = { index, item -> item.id }) { index, item ->
-			var focused by remember { mutableStateOf(false) }
-			val focusRequester = remember { FocusRequester() }
-			val animScale = animateFloatAsState(if (focused) 1.2f else 1f)
+    LaunchedEffect(viewHasFocused) {
+        if (!viewHasFocused) return@LaunchedEffect
+        val visibleItems = snapshotFlow { lazyRowState.layoutInfo.visibleItemsInfo }
+            .filter { it.isNotEmpty() }
+            .first()
 
-			Card(
-				modifier = Modifier
-					.graphicsLayer {
-						scaleX = animScale.value
-						scaleY = animScale.value
-					}
-					.padding(start = if (index == 0) 40.dp else 0.dp, end = if (index == itemsToPlayStateList.lastIndex) 40.dp else 0.dp)
-					.padding(horizontal = 8.dp)
-					.onFocusChanged {
-						focused = it.isFocused
-					}
-					.focusRequester(focusRequester)
-					.focusable()
-					.onKeyEvent { keyEvent ->
-						if (keyEvent.key == Key.Back || keyEvent.key == Key.Escape) {
-							true
-						}
-						if ((keyEvent.key == Key.Enter || keyEvent.key == Key.DirectionCenter) && keyEvent.type == KeyEventType.KeyUp) {
-							onItemClicked(index, itemsToPlayStateList.toList())
-							true
-						} else {
-							false
-						}
-					},
-				shape = RoundedCornerShape(8.dp),
-				colors = CardDefaults.cardColors(containerColor = Color.Transparent)
-			) {
-				val spi by remember {
-					mutableStateOf(
-						item.itemImages.takeIf { it.isNotEmpty() }?.get(org.jellyfin.sdk.model.api.ImageType.PRIMARY)?.getUrl(api)
-					)
-				}
-				AsyncImage(
-					model = ImageRequest.Builder(context)
-						.diskCachePolicy(CachePolicy.ENABLED)
-						.data(spi)
-						.build(),
-					contentDescription = "",
-					modifier = Modifier
-						.size(140.dp, 80.dp)
-						.clip(RoundedCornerShape(8.dp)),
-					contentScale = ContentScale.Crop
-				)
-				Spacer(modifier = Modifier.height(2.dp))
+        val targetIndex = if (visibleItems.any { it.key == currentItem.id }) {
+            visibleItems.first { it.key == currentItem.id }.index
+        } else if (visibleItems.any { it.index == currentFocusedIndex }) {
+            currentFocusedIndex
+        } else {
+            lazyRowState.firstVisibleItemIndex
+        }
+        focusRequesterStateList[targetIndex].requestFocus()
+    }
 
-				val itemName = item.name
-				val s = item.parentIndexNumber?.toString()?.let { "S${it.padStart(2, '0')}" }
-				val e = item.indexNumber?.toString()?.let { "E${it.padStart(2, '0')}" }
-				val se = if (s != null && e != null) "$s$e: " else if (s != null) "$s: " else if (e != null) "$e: " else ""
-				Text(
-					text = "${se}${itemName}",
-					color =  if (item.id == currentItem.id) composeColor else Color.White,
-					modifier = Modifier
-						.widthIn(max = 140.dp)
-						.height(42.dp),
-					overflow = TextOverflow.Ellipsis,
-					maxLines = 2,
-					fontSize = 14.sp
-				)
-			}
+    LazyRow(
+        modifier = modifier
+            .focusGroup()
+            .focusable(),
+        state = lazyRowState
+    ) {
+        itemsIndexed(
+            itemsToPlayStateList,
+            key = { index, item -> item.id },
+            contentType = { index, item -> item.id }) { index, item ->
+            var focused by remember { mutableStateOf(false) }
+            val animScale = animateFloatAsState(if (focused) 1.2f else 1f)
 
-			LaunchedEffect(viewHasFocused) {
-				if (viewHasFocused && index == lazyRowState.firstVisibleItemIndex && first) {
-					focusRequester.requestFocus()
-				}
-			}
-		}
-	}
+            Card(
+                modifier = Modifier
+                    .graphicsLayer {
+                        scaleX = animScale.value
+                        scaleY = animScale.value
+                    }
+                    .padding(
+                        start = if (index == 0) 40.dp else 0.dp,
+                        end = if (index == itemsToPlayStateList.lastIndex) 40.dp else 0.dp
+                    )
+                    .padding(horizontal = 8.dp)
+                    .onFocusChanged {
+                        focused = it.isFocused
+                        if (it.isFocused) {
+                            currentFocusedIndex = index
+                        }
+                    }
+                    .focusRequester(focusRequesterStateList[index])
+                    .focusable()
+                    .onKeyEvent { keyEvent ->
+                        if (keyEvent.key == Key.Back || keyEvent.key == Key.Escape) {
+                            true
+                        }
+                        if ((keyEvent.key == Key.Enter || keyEvent.key == Key.DirectionCenter) && keyEvent.type == KeyEventType.KeyUp) {
+                            onItemClicked(index, itemsToPlayStateList.toList())
+                            true
+                        } else {
+                            false
+                        }
+                    },
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.Transparent)
+            ) {
+                val spi by remember {
+                    mutableStateOf(
+                        item.itemImages.takeIf { it.isNotEmpty() }
+                            ?.get(org.jellyfin.sdk.model.api.ImageType.PRIMARY)?.getUrl(api)
+                    )
+                }
+                AsyncImage(
+                    model = ImageRequest.Builder(context)
+                        .diskCachePolicy(CachePolicy.ENABLED)
+                        .data(spi)
+                        .build(),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(140.dp, 80.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+
+                val itemName = item.name
+                val s = item.parentIndexNumber?.toString()?.let { "S${it.padStart(2, '0')}" }
+                val e = item.indexNumber?.toString()?.let { "E${it.padStart(2, '0')}" }
+                val se =
+                    if (s != null && e != null) "$s$e: " else if (s != null) "$s: " else if (e != null) "$e: " else ""
+                Text(
+                    text = "${se}${itemName}",
+                    modifier = Modifier
+                        .widthIn(max = 140.dp)
+                        .height(42.dp),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2,
+                    style = MaterialTheme
+                        .typography
+                        .bodyMedium
+                        .copy(color = if (item.id == currentItem.id) composeColor else Color.White)
+                )
+            }
+        }
+    }
 }
